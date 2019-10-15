319ffe2e8e62b52a4cdad185d536d00ea7653617
Mr-Personne <h.sacha07@gmail.com>
2019-10-15 11:58:24 +0000

    rend le bg du bouton droite rouge pour tester

--- slider.js
+++ slider.js
@@ -1,6 +1,6 @@
 function test(){
     // alert("test");
-    var bouton = document.getElementById("right_butt");
+    var bouton = document.getElementById("right-button");
     bouton.style.backgroundColor = "red";
     
 }
