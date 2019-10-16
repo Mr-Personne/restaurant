577799d5ded75dc4c99a9a6fa5574c0e47e0d5b4
Mr-Personne <h.sacha07@gmail.com>
2019-10-16 14:14:48 +0000

    j'ai enlever la fonction inutile

--- slider.js
+++ slider.js
@@ -1,4 +1,3 @@
-
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -10,31 +9,19 @@ function currentSlide(n) {
     showSlides(slideIndex = n);
 }
 
-function slideLeft() {
-    // alert("test");
-    var slide1 = document.getElementsByClassName("slide1");
-    var slide2 = document.getElementsByClassName("slide2");
-    alert(slide2[0]);
-    slide1[0].style.display = "none";
-    slide2[0].style.display = "flex";
-
-}
 
 
 function showSlides(n) {
-    //alert("test right");
     var i;
     var slides = document.getElementsByClassName("inner-slide-d-flex");
-    //var dots = document.getElementsByClassName("dot");
+    
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    /* for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-     */
+    
+    
     slides[slideIndex - 1].style.display = "flex";
-    //dots[slideIndex - 1].className += " active";
+    
 }
