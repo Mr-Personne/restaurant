e42c47976f314233df003ad0233b824c3b522672
Mr-Personne <h.sacha07@gmail.com>
2019-10-16 06:45:46 +0000

    effet slider mis en place (w3school slider)

--- slider.js
+++ slider.js
@@ -1,16 +1,40 @@
-function slideLeft(){
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function slideLeft() {
     // alert("test");
     var slide1 = document.getElementsByClassName("slide1");
     var slide2 = document.getElementsByClassName("slide2");
     alert(slide2[0]);
     slide1[0].style.display = "none";
     slide2[0].style.display = "flex";
-    
+
 }
 
-function slideRight(){
-    // alert("test");
-    var bouton = document.getElementById("left-button");
-    bouton.style.backgroundColor = "red";
-    
+
+function showSlides(n) {
+    //alert("test right");
+    var i;
+    var slides = document.getElementsByClassName("inner-slide-d-flex");
+    //var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    /* for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+     */
+    slides[slideIndex - 1].style.display = "flex";
+    //dots[slideIndex - 1].className += " active";
 }
