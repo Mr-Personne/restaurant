1cf39f451ddb9cc42a66df3ccc289362ddf7bd81
Mr-Personne <h.sacha07@gmail.com>
2019-10-17 10:39:36 +0000

    enleve fonction inutile

--- slider.js
+++ slider.js
@@ -10,15 +10,7 @@ function currentSlide(n) {
     showSlides(slideIndex = n);
 }
 
-function slideLeft() {
-    // alert("test");
-    var slide1 = document.getElementsByClassName("slide1");
-    var slide2 = document.getElementsByClassName("slide2");
-    alert(slide2[0]);
-    slide1[0].style.display = "none";
-    slide2[0].style.display = "flex";
 
-}
 
 
 function showSlides(n) {
