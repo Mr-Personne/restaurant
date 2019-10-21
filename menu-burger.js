b0559f5eacd354d896b8b1c15f28ef73f6f80b1f
Mr-Personne <h.sacha07@gmail.com>
2019-10-21 15:20:59 +0000

    onresize sur index, css what?, menu-burger onresize stuff

--- menu-burger.js
+++ menu-burger.js
@@ -14,25 +14,46 @@ console.log(headerHeight);
 var navHeight = bodyHeight - headerHeight;
 console.log(navHeight);
 
+//calcule width de la fenetre pour savoir quel style il faut appliquer dans le onresize
+var screenWidth = screen.width;
 
 
-function openNav(){
+
+function openNav() {
     // onOff = 1;
-    
     var navMenu = document.getElementById("navi");
 
     //applique taille navHeight au css du nav
-    navMenu.style.height = navHeight+"px";
+    navMenu.style.height = navHeight + "px";
 
-    if(onOff == 0){
+    if (onOff == 0) {
         navMenu.style.display = "block";
         navMenu.style.position = "absolute";
         onOff = 1;
     }
-    else if(onOff == 1){
+    else if (onOff == 1) {
         navMenu.style.display = "none";
+        navMenu.style.position = "initial";
+        navMenu.style.height = "100%";
         onOff = 0;
     }
-    
+
     //alert("burger");
+
+}
+
+
+function myFunction() {
+    if (screenWidth >= 768) {
+        var navMenu = document.getElementById("navi");
+        navMenu.style.display = "block";
+        navMenu.style.position = "initial";
+        navMenu.style.height = "100%";
+    }
+    else {
+        navMenu.style.display = "none";
+        //applique taille navHeight au css du nav
+        navMenu.style.height = navHeight + "px";
+        navMenu.style.position = "absolute";
+    }
 }
